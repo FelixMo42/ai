b8129f78d8eee37ab579638ed409ba4040fd142a
FelixMo42 <felix@tacocat.com>
2018-01-31 16:56:17 -0800

    ai funs setup - actFunc

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-inputSize = 784
-middleSize = 32
-outputSize = 10
+inputs = 784
+middles = 32
+outputs = 10
 
-sampleSize = 200
-doSize = 50
+samples = 100
+doSize = 20
+
+rate = .25
 
 #import data
 
@@ -13,7 +15,6 @@ import random
 import numpy as np
 
 path = os.path.dirname( os.path.realpath(__file__) )
-answer = 0
 
 testingDataCSV = csv.reader( open(path + "/mnist_test.csv", "r") )
 testingData = []
@@ -21,10 +22,84 @@ testingData = []
 for row in testingDataCSV:
     testingData.append( [int(i) for i in row] )
 
-d = testingData
+data = testingData
 
-#ai code
+#ai vars
 
+a = [
+    np.zeros( inputs ),
+    np.zeros( outputs )
+]
+
+w = [
+    np.random.rand(inputs, outputs)
+]
+
+#ai func
+
+def actFunc(x):
+    return x
+
+#ai interface
+
+def foward():
+    for t in range(outputs):
+        a[1][t] = 0
+        for n in range(inputs):
+            a[1][t] += a[0][n] * w[0][n][t]
+        a[1][t] = actFunc(a[1][t])
+
+def back():
+    for t in range(outputs):
+        for n in range(inputs):
+            if a[0][n] > 0:
+                if (t == answer):
+                    c = a[0][n]
+                else:
+                    c = -a[0][n]
+                w[0][n][t] = min( max( w[0][n][t] + c * rate , 0 ) , 1 )
+
+#io
+
+answer = 0
+
+def input(inpt):
+    global answer
+    answer = data[inpt][0]
+
+    for i in range(inputs):
+        a[0][i] = data[inpt][i + 1] / 255
+
+def output():
+    m = -1
+    r = -1
+
+    for i in range(outputs):
+        if a[1][i] > m:
+            r = i
+            m = a[1][i]
+
+    return r
+
+#main loop
+
+for n in range(doSize):
+    correct = 0
+
+    for i in range(samples):
+        input( random.randint( 0, len(data) - 1 ) )
+        foward()
+        back()
+        if output() == answer:
+            correct += 1
+
+        #print(a)
+
+    print( str(n + 1) + ": " + str(correct) + " / " + str(samples) + " correct" )
+
+
+
+'''
 
 a = [
     np.zeros( inputSize ),
@@ -70,7 +145,7 @@ def output():
 
     return r
 
-
+'''
 
 
 
